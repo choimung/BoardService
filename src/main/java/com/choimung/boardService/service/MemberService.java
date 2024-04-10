809b6649896fa4a38bca79fabfaa4830af38c1b7
choimung <103022503+choimung@users.noreply.github.com>
2024-04-10 17:38:01 +0900

    feat : 도메인 Service 생성

---
+++ src/main/java/com/choimung/boardService/service/MemberService.java
@@ -0,0 +1,26 @@
+package com.choimung.boardService.service;
+
+import com.choimung.boardService.domain.Member;
+import com.choimung.boardService.respository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public Long join(Member member) {
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    public Member login(String loginId, String password) {
+        return memberRepository.findByLoginId(loginId).stream()
+                .filter(m -> m.getPassword().equals(password)).findFirst()
+                .orElse(null);
+    }
+}
---
+++ src/main/java/com/choimung/boardService/service/PostsService.java
@@ -0,0 +1,25 @@
+package com.choimung.boardService.service;
+
+import com.choimung.boardService.domain.Posts;
+import com.choimung.boardService.respository.PostsRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    public void save(Posts posts) {
+        postsRepository.save(posts);
+    }
+
+    public List<Posts> findAll() {
+        return postsRepository.findAll();
+    }
+}
